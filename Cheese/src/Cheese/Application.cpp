1fa1b2900d709302b6c14a8ffe2629284318fdc6
chaser <bjmxyt@outlook.com>
2022-04-17 18:52:07 +0800

    Window Resize Events.

--- Cheese/src/Cheese/Application.cpp
+++ Cheese/src/Cheese/Application.cpp
@@ -48,6 +48,7 @@ namespace Cheese {
 	{
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		dispatcher.Dispatch<WindowResizeEvent>(BIND_EVENT_FN(OnWindowResize));
 
 		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
 		{
@@ -65,8 +66,11 @@ namespace Cheese {
 			Timestep timestep = time - m_LastFrameTime;
 			m_LastFrameTime = time;
 
-			for (Layer* layer : m_LayerStack)
-				layer->OnUpdate(timestep);
+			if (!m_Minimized)
+			{
+				for (Layer* layer : m_LayerStack)
+					layer->OnUpdate(timestep);
+			}
 
 			m_ImGuiLayer->Begin();
 			for (Layer* layer : m_LayerStack)
@@ -83,4 +87,17 @@ namespace Cheese {
 		return true;
 	}
 
+	bool Application::OnWindowResize(WindowResizeEvent& e)
+	{
+		if (e.GetWidth() == 0 || e.GetHeight() == 0)
+		{
+			m_Minimized = true;
+			return false;
+		}
+
+		m_Minimized = false;
+		Renderer::OnWindowResize(e.GetWidth(), e.GetHeight());
+
+		return false;
+	}
 }
--- Cheese/src/Cheese/Renderer/Renderer.cpp
+++ Cheese/src/Cheese/Renderer/Renderer.cpp
@@ -17,6 +17,11 @@ namespace Cheese {
 		RenderCommand::Init();
 	}
 
+	void Renderer::OnWindowResize(uint32_t width, uint32_t height)
+	{
+		RenderCommand::SetViewport(0, 0, width, height);
+
+	}
 	void Renderer::EndScene()
 	{
 	}
